fe76c709a6b32f0b291e4b2549ffd1888b3c31b6
suraj2045 <sdabhade101@gmail.com>
2023-05-22 17:45:38 +0530

    v2

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ const app = express();
 require("./db/conn");
 const mysql = require('mysql2/promise');
 const cors = require("cors");
-const router = require("./routes/router")
+// const router = require("./routes/router")
 const register = require("./routes/register")
 const procedures = require("./routes/procedures")
-<<<<<<< HEAD
 const doctor = require("./routes/doctors")
 // const logging= require("./routes/logging")
-=======
-const review = require("./routes/review")
->>>>>>> e266354cb297e39b3d03db385dde106caa94985e
 const port = 8004;
 
 app.use(express.json());
@@ -20,18 +16,11 @@ app.use(cors());
 
 app.use("/uploads",express.static("./uploads"))
 
-<<<<<<< HEAD
-app.use(router);
+// app.use(router);
 app.use(register);
 app.use(procedures);
 app.use(doctor);
 // app.use(logging);
-=======
-app.use(router)
-app.use(register)
-app.use(procedures)
-app.use(review)
->>>>>>> e266354cb297e39b3d03db385dde106caa94985e
 
 app.listen(port,()=>{
     console.log("server start",port)
